0e0407e002cce8bc4fe858cde6f75eea7e2df911
LAPTOP-K22GA1O8 <pranjalgangwar1807@gmail.com>
2024-04-21 20:06:00 +0530

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ mongoose
   })
   .catch((err) => console.log(err));
 app.get("/", () => {
-  console.log("server running");
+  res.send("server running");
 });
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
